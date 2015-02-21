85996b738ade62536bc7abbdec50ecb60980ea1a
Jeffrey Qua <jeffrey.qua@gmail.com>
2015-02-20 16:42:22 -0800

    Cleaned up code and added comments

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # spotify-test
-
+# Jeffrey Qua - 20 Feb 2015
 
 Some reference code for spotify form from:
 http://jsfiddle.net/JMPerez/0u0v7e1b/
+
+Architecture -->
+I opted for a HTML/JS single-page application as that seemed to be the most efficient use of time to write code without having to use a framework.
+The required functionality of API/JS calls are implemented but I did not have enough time for more additional suggested features such as routing or pagination.
+
+A large portion of the code was improvised from the Spotify API call example listed above. Scripts included Jquery, and Handlebars for templating. Ideally would have used some type of framework as a coding build such as grunt to apply advanced features, as well as using additional css frameworks, ie. sass.
+
+Styling -->
+The majority of my time was prioritized for functionality but what styling I did apply was minimal inspired by the current Spotify web interface - elements such as color, fonts, and formatting of artist results list and track results list.
+
+Limitations -->
+Styling considerations was only given to a standard desktop browser.
+Known issues:
+-track list is not visible at smaller window heights (need to add scrolling)
--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // scripts.js
 // JS Functions
-// find template and compile it
+
 var templateSource = document.getElementById('artist-results-template').innerHTML,
     template = Handlebars.compile(templateSource),
     trackTemplateSource = document.getElementById('track-results-template').innerHTML,
@@ -8,19 +8,9 @@ var templateSource = document.getElementById('artist-results-template').innerHTM
     loadingSource = document.getElementById('loading-template').innerHTML,
     loadingTemplate = Handlebars.compile(loadingSource),
     resultsPlaceholder = document.getElementById('results'),
-    trackResults = document.getElementById('tracks'),
-    playingCssClass = 'playing',
-    audioObject = null;
+    trackResults = document.getElementById('tracks');
 
-var fetchTracks = function (albumId, callback) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            callback(response);
-        }
-    });
-};
-
+// Example function code from referenced example code (readme)
 var searchAlbums = function (query) {
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
@@ -34,6 +24,7 @@ var searchAlbums = function (query) {
     });
 };
 
+// Search Artist Name - Spotify API Call
 var searchArtists = function (query) {
   resultsPlaceholder.innerHTML = loadingTemplate();
     $.ajax({
@@ -49,6 +40,7 @@ var searchArtists = function (query) {
     });
 };
 
+// Search Artist Top Tracks - Spotify API Call
 var searchArtistsTracks = function (query) {
     $.ajax({
         url: 'https://api.spotify.com/v1/artists/'+query+'/top-tracks',
@@ -58,13 +50,14 @@ var searchArtistsTracks = function (query) {
         success: function (response) {
           console.log(response);
             trackResults.innerHTML = trackTemplate(response);
-            // trackResults.className='';
+            // Add overlay and show track window
             $('#overlay').addClass('active');
             $('#tracks').removeClass('hidden');
         }
     });
 };
 
+// Artist Link Trigger - opens up Top Tracks Panel
 document.getElementById('results').addEventListener('click', function (e) {
   var target = e.target.parentNode;
   if (target !== null && target.classList.contains('artist_box')) {
@@ -73,14 +66,14 @@ document.getElementById('results').addEventListener('click', function (e) {
   }
 });
 
-
+// Overlay Listener - Hides Top Tracks Panel
 document.getElementById('overlay').addEventListener('click', function (e) {
   // Hide overlay on click
   $('#overlay').removeClass('active');
   $('#tracks').addClass('hidden');
 });
 
-
+// Search button Submit
 document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault();
     searchArtists(document.getElementById('query').value);
